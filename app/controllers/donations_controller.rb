427c40bdad500e953448c9d4389beb69de8179c6
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-19 13:52:13 -0500

    create donation controller, index route, and seeds

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,23 @@
+class DonationsController < ApplicationController
+
+
+  def index 
+    @donations = Donation.all 
+    render :index 
+  end 
+
+  # def create
+  #   @donation = Donation.create(
+  #     body: params[:body],
+  #     user_id: params[:user_id]
+  #   )
+  #   render :show
+  # end
+
+  # def destroy
+  #   @donation = Donation.find_by(id: params[:id])
+  #   @donation.destroy
+  #   render json: { message: "Your comment on the donation page is deleted" }
+  # end
+
+end
